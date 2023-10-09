fe87fb8a5dd812d212bc43545dbaf3d6a5b533b4
lcfd <9001053+fedriz@users.noreply.github.com>
2023-10-09 13:22:03 +0200

    Add new config py section

---
+++ cli/src/trak/config.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+
+#
+# Paths
+#
+
+
+TRAK_FOLDER = Path.home() / ".trak"
+
+DB_FILE_PATH = TRAK_FOLDER / "db.json"
+CONFIG_FILE_PATH = TRAK_FOLDER / "config.json"
+
+
+#
+# Configuration helpers
+#
+
+
+def init_config(p: Path) -> int:
+    """Create the to-do database."""
+
+    try:
+        p.parent.mkdir(parents=True, exist_ok=True)
+        with p.open("w", encoding="utf-8") as f:
+            f.write("{}")
+        return 0
+    except OSError:
+        return 1
