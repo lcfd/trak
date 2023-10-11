68f5cebccd6236a69a40f2658132cf5aae844fcf
Marco Burro <marcoburro98@gmail.com>
2023-10-11 12:57:26 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        pkgs = nixpkgs.legacyPackages.${system};
+        cli = mkPoetryApplication {
+          projectDir = ./cli;
+          preferWheels = true;
+          python = pkgs.python311;
+          checkGroups = [ ];
+        };
+      in
+      {
+        packages = {
+          default = cli;
+        };
+      }
+    );
+}
