9f8d6b2eb13af4052c4de6937e923210cd90abc5
lcfd <9001053+fedriz@users.noreply.github.com>
2023-12-13 12:29:45 +0100

    Add works delete command

--- cli/trakcli/works/__init__.py
+++ cli/trakcli/works/__init__.py
@@ -1,9 +1,10 @@
 import typer
 
+from trakcli.works.commands.delete import delete_work
 from trakcli.works.commands.list import list_works
-
 
 app = typer.Typer()
 
 
 app.command("list")(list_works)
+app.command("delete")(delete_work)
