eae48414108eafc22d436179c3bdbed4f7869b72
lcfd <9001053+fedriz@users.noreply.github.com>
2024-03-24 16:52:50 +0100

    Add a style for Select Questionary

---
+++ cli/trakcli/utils/styles_questionary.py
@@ -0,0 +1,12 @@
+from questionary import Style
+
+questionary_style_select = Style(
+    [
+        ("pointer", "fg:#673ab7 bold"),
+        (
+            "highlighted",
+            "fg:#673ab7 bold",
+        ),
+        ("selected", "fg:#cc5454"),
+    ]
+)
