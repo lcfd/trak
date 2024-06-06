d1070e856573284a0af2b53dba7c6654a1400561
lcfd <9001053+fedriz@users.noreply.github.com>
2024-06-06 22:01:50 +0200

    Add create projects folder on init

--- cli/trakcli/config/main.py
+++ cli/trakcli/config/main.py
@@ -13,6 +13,7 @@ TRAK_FOLDER = Path.home() / ".trak"
 DB_FILE_PATH = TRAK_FOLDER / "db.json"
 DEV_DB_FILE_PATH = TRAK_FOLDER / "dev_db.json"
 CONFIG_FILE_PATH = TRAK_FOLDER / "config.json"
+PROJECT_FOLDER_PATH = TRAK_FOLDER / "projects/"
 
 
 # Read the config at CONFIG_FILE_PATH
@@ -23,25 +24,3 @@ def get_config():
 def get_db_file_path():
     CONFIG = get_config()
     return DEV_DB_FILE_PATH if CONFIG.get("development", False) else DB_FILE_PATH
-
-
-#
-# Configuration helpers
-#
-
-
-def init_config(p: Path) -> int:
-    """Init the config file."""
-
-    try:
-        p.parent.mkdir(parents=True, exist_ok=True)
-        with p.open("w", encoding="utf-8") as db:
-            json.dump(
-                {"development": False, "projects": []},
-                db,
-                indent=2,
-                separators=(",", ": "),
-            )
-        return 0
-    except OSError:
-        return 1
