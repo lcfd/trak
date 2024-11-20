7a794a31b85f16ed2584cbf49af2aa07e15fd252
lcfd <9001053+fedriz@users.noreply.github.com>
2024-11-20 22:10:03 +0100

    Improve main callback

--- trak/main.py
+++ trak/main.py
@@ -14,18 +14,17 @@ from trak.configs.commands import app as config_app
 from trak.create import app as create_app
 from trak.delete import app as delete_app
 from trak.dev.commands import app as dev_app
+from trak.doctor import app as doctor_app
 from trak.initialize import initialize_trak
-
-# from trak.projects import app as projects_app
 from trak.report import app as report_app
 from trak.tracker.commands.get_current_session_status import (
-    get_current_session_status,
+    command_status,
 )
 from trak.tracker.commands.start_tracker import start_tracker
 from trak.tracker.commands.stop_tracker import stop_tracker
 
+# from trak.projects import app as projects_app
 # from trak.works import app as works_app
-from trak.doctor import app as doctor_app
 
 app = typer.Typer()
 
@@ -34,13 +33,31 @@ app = typer.Typer()
 
 initialize_trak()
 
+
+@app.callback()
+def main(
+    version: Optional[bool] = MainVersionOption,
+    website: Optional[bool] = MainWebsiteOption,
+    repository: Optional[bool] = MainRepositoryOption,
+    issues: Optional[bool] = MainIssuesOption,
+    bug: Optional[bool] = MainBugOption,
+    docs: Optional[bool] = MainDocsOption,
+) -> None:
+    """
+    Keep a record of the time you dedicate to your projects.
+    """
+
+    if version or website or repository or issues or bug or docs:
+        raise typer.Exit()
+
+
 # issue-89
 
 # On the actions
 app.command(name="start", help="Start a session.")(start_tracker)
 app.command(name="stop", help="Stop the current session.")(stop_tracker)
 app.command(name="status", help="Show the status of the current session.")(
-    get_current_session_status
+    command_status
 )
 
 # Write actions
@@ -60,22 +77,6 @@ app.add_typer(doctor_app, name="doctor", help="Keep your trak instance in shape.
 app.add_typer(dev_app, name="dev", help="Utils for trak developers.")
 
 
-#
 # Add subcommands
 # app.add_typer(projects_app, name="projects", help="Interact with your projects.")
 # app.add_typer(works_app, name="works", help="Interact with your works.")
-
-#
-# Actions routes
-
-
-@app.callback()
-def main(
-    version: Optional[bool] = MainVersionOption,
-    website: Optional[bool] = MainWebsiteOption,
-    repository: Optional[bool] = MainRepositoryOption,
-    issues: Optional[bool] = MainIssuesOption,
-    bug: Optional[bool] = MainBugOption,
-    docs: Optional[bool] = MainDocsOption,
-) -> None:
-    return
