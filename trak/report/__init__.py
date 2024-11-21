fbaf028a089056802c0b08dcc42fc1d1aab4db27
lcfd <9001053+fedriz@users.noreply.github.com>
2024-11-21 04:58:43 +0100

    Move commands outside folder

--- trak/report/__init__.py
+++ trak/report/__init__.py
@@ -1,8 +1,8 @@
 import typer
 
-from trak.report.commands.project import report_project
-from trak.report.commands.sessions import report_sessions
-from trak.report.commands.works import report_works
+from trak.report.project import report_project
+from trak.report.sessions import report_sessions
+from trak.report.works import report_works
 
 app = typer.Typer()
 
