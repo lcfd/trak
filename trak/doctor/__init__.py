d69d7b02f0b2fc14e46eef32f1355708af966ae1
lcfd <9001053+fedriz@users.noreply.github.com>
2024-11-30 12:10:19 +0100

    Add works doctor command

--- trak/doctor/__init__.py
+++ trak/doctor/__init__.py
@@ -1,9 +1,10 @@
 import typer
 
-from trak.doctor.commands import doctor_config, doctor_sessions
+from trak.doctor.commands import doctor_config, doctor_sessions, doctor_works
 
 app = typer.Typer()
 
 
 app.command("config", help="Check if the config is ok.")(doctor_config)
 app.command("sessions", help="Check if the sessions are ok.")(doctor_sessions)
+app.command("works", help="Check if the works are ok.")(doctor_works)
--- trak/doctor/commands.py
+++ trak/doctor/commands.py
@@ -1,8 +1,13 @@
+import json
+import pathlib
+from json import JSONDecodeError
+
 from trak.config import get_config, get_db_file_path
 from trak.database import get_db_content
 from trak.models import Config
-from trak.paths import CONFIG_FILE_PATH, DB_FILE_PATH
+from trak.paths import CONFIG_FILE_PATH, DB_FILE_PATH, PROJECTS_FOLDER_PATH
 from trak.utils.base_messages import print_error, print_success
+from trak.works.models import Work
 
 
 def doctor_config():
@@ -62,4 +67,49 @@ def doctor_sessions():
         print_success(
             title="Your database is in shape",
             text=f"You can also check it out manually at {DB_FILE_PATH}",
+        )
+
+
+def doctor_works():
+    something_wrong = False
+
+    projects_path = pathlib.Path(PROJECTS_FOLDER_PATH)
+
+    for x in projects_path.iterdir():
+        if not x.is_dir():
+            continue
+
+        works_in_project_path = x / "works.json"
+        with open(works_in_project_path, "r") as f:
+            try:
+                works_from_json = json.load(f)
+                list(map(lambda w: Work(**w), works_from_json))
+            except TypeError as e:
+                keyword_error_marker = (
+                    "Work.__new__() got an unexpected keyword argument"
+                )
+                if keyword_error_marker in str(e):
+                    print_error(
+                        title=f"Wrong fields in {x}",
+                        text=f"Wrong field: {str(e).split(" unexpected keyword argument ")[1]}.",
+                    )
+                something_wrong = True
+                continue
+            except JSONDecodeError:
+                print_error(
+                    title=f"Wrong work in {x}",
+                    text="There is one or more works that aren't in Json format.",
+                )
+                continue
+            except Exception:
+                print_error(
+                    title="Something wrong",
+                    text='Create an issue on GitHub! \n Use the "trak -i" command the get the link.',
+                )
+                continue
+
+    if not something_wrong:
+        print_success(
+            title="Your works are shape",
+            text=f"You can also check them out manually at {PROJECTS_FOLDER_PATH}",
         )
