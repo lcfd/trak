a6d5d38bd72ebf90fffe4fb489fd4cf222d80988
lcfd <9001053+fedriz@users.noreply.github.com>
2024-12-01 20:37:48 +0100

    Improve dates utils

--- trak/utils/dates.py
+++ trak/utils/dates.py
@@ -1,20 +1,37 @@
 from datetime import datetime
 
 
-def datetime_to_string(dt: datetime):
-    """Converta date to string with %Y-%m-%dT%H:%M format."""
-    return dt.strftime("%Y-%m-%dT%H:%M")
+def datetime_to_string(dt: datetime | str):
+    """
+    Converta date to string with %Y-%m-%dT%H:%M format.
+    """
+
+    if isinstance(dt, datetime):
+        return dt.strftime("%Y-%m-%dT%H:%M")
+    if isinstance(dt, str):
+        return dt
 
 
-def format_date(str_date: str):
-    """Format string date to %Y-%m-%d, %H:%M."""
+def format_datetime_readable(str_date: str):
+    """
+    Format string date to %Y-%m-%d, %H:%M.
+    """
+
     d = datetime.fromisoformat(str_date)
     return d.strftime("%Y-%m-%d, %H:%M")
 
 
-def same_week(dateString):
-    """Returns true if a dateString in %Y%m%d format is part of the current week."""
+def same_week(dateString: str):
+    """
+    Returns true if a dateString in %Y%m%d format is part of the current week.
+    """
 
-    d1 = datetime.strptime(dateString, "%Y%m%d")
+    try:
+        d1 = datetime.strptime(dateString, "%Y%m%d")
+    except ValueError:
+        print("Wrong value provided.")
+        return False
+
     d2 = datetime.today()
+
     return d1.isocalendar()[1] == d2.isocalendar()[1] and d1.year == d2.year
