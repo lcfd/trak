5e4d4ef2e8bed58644b4876d61b2d981d81b88eb
lcfd <9001053+fedriz@users.noreply.github.com>
2025-01-27 09:18:54 +0100

    Add comment

--- trak/utils/time.py
+++ trak/utils/time.py
@@ -1,4 +1,6 @@
 def get_hours_minutes_from_seconds(seconds: int) -> tuple[int, int]:
+    """Transforms seconds into a hours and minutes tuple."""
+
     m, _ = divmod(seconds, 60)
     h, m = divmod(m, 60)
 
